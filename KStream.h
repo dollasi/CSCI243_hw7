e7639f014a54e59d4a007f4406a710d54a2f2f3b
Alexandria Lynne Jones <alj5312@rit.edu>
2025-11-15 09:24:53 -0500

    add Kstream header w interface design

---
+++ KStream.h
@@ -0,0 +1,15 @@
+//author: Alexandria Jones
+
+#ifndef KSTREAM_H
+#define KSTREAM_H
+#include <stddef.h>
+
+typedef struct KStream_s* Kstream;
+
+KStream ks_create(File* key_fp);
+
+void ks_destroy(KStream ks);
+
+void ks_translate(KStream ks, const unsigned char* in, unsigned char* out, size_t len);
+
+#endif  
